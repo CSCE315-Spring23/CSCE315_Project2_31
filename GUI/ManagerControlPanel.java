1443daace222939572ffac5f323395f02778ebac
Atsubyo <art.tyoung@tamu.edu>
2023-03-20 13:16:10 -0500

    Created default template for report buttons

--- GUI/ManagerControlPanel.java
+++ GUI/ManagerControlPanel.java
@@ -10,18 +10,18 @@ public class ManagerControlPanel {
         // Set the layout manager to FlowLayout with left alignment
         this.panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        // Create some buttons and add them to the panel
+        // X Report generation
         JButton getXReport = new JButton("X Report");
         getXReport.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 double salesTotal = db.generateXReport(1);
-                JOptionPane.showMessageDialog(panel, "Sales total: " + salesTotal);
+                JOptionPane.showMessageDialog(panel, String.format("Sales total: $%.2f", salesTotal));
             }
         });
 
+        // Z Report generation
         JButton getZReport = new JButton("Z Report");
-
         getZReport.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -34,11 +34,37 @@ public class ManagerControlPanel {
             }
         });
 
+        // Sales Report generation
+        JButton getSalesReport = new JButton("Sales Report");
+        getSalesReport.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // Excess Report generation
         JButton getExcessReport = new JButton("Excess Report");
+        getExcessReport.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // Restock Report generation
         JButton getRestockReport = new JButton("Restock Report");
+        getRestockReport.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
         // TODO: Implement the functionality of the get_Report buttons
         this.panel.add(getXReport);
         this.panel.add(getZReport);
+        this.panel.add(getSalesReport);
         this.panel.add(getExcessReport);
         this.panel.add(getRestockReport);
 
